697015583f30bdc3a1fe94714d7b8a707030537c
Martin <qule520@126.com>
2026-05-08 23:21:32 +0800

    test: TestBM25Tokenizer->TestTokenizer

---
+++ test-unit/TestTokenizer.cs
@@ -0,0 +1,33 @@
+﻿using FileReaderMcpServer.Search;
+
+namespace TestUnit
+{
+    [TestClass]
+    public sealed class TestTokenizer: TestBase
+    {
+        [TestMethod]
+        public void TestTokenizerZh()
+        {
+            var content = @"我只是一个过客，我的名字叫Martin";
+            var tokens = Tokenizer.Tokenize(content, "zh");
+            var expectedTokens = new List<string> { "我", "只是", "一个", "过客", "，", "我", "的", "名字", "叫", "martin" };
+            CollectionAssert.AreEqual(expectedTokens, tokens);
+        }
+        [TestMethod]
+        public void TestTokenizerJa()
+        {
+            var content = @"私はただの通りすがりです。私の名前はMartinです。";
+            var tokens = Tokenizer.Tokenize(content, "ja");
+            var expectedTokens = new List<string> { "私", "は", "ただ", "の", "通りすがり", "です", "。", "私", "の", "名前", "は", "martin", "です", "。" };
+            CollectionAssert.AreEqual(expectedTokens, tokens);
+        }
+        [TestMethod]
+        public void TestTokenizerEn()
+        {
+            var content = @"I am just a passerby";
+            var tokens = Tokenizer.Tokenize(content, "en");
+            var expectedTokens = new List<string> { "i", "am", "just", "passerbi" };
+            CollectionAssert.AreEqual(expectedTokens, tokens);
+        }
+    }
+}
